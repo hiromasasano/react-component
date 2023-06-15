cd9719290d5117ce3165c0c32b522c498b71dcc4
HS <hsano@comsis.co.jp>
2023-06-15 00:05:08 +0000

    Update 7 files

---
+++ src/components/organisms/user/UserCard.jsx
@@ -0,0 +1,18 @@
+export const UserCard = () => {
+  return (
+    <div>
+      <img alt="プロフィール" />
+      <p>名前</p>
+      <dl>
+        <dl>メール</dl>
+        <dd>11111@aaa.com</dd>
+        <dl>TEL</dl>
+        <dd>000-9999-88888</dd>
+        <dl>会社名</dl>
+        <dd>あああああ会社</dd>
+        <dl>WEB</dl>
+        <dd>httttttt.com</dd>
+      </dl>
+    </div>
+  );
+};
